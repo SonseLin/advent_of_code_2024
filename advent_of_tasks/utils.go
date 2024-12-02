904069a177a1573403b001be16905ebd58ae1722
Sonse <sonselinao@gmail.com>
2024-12-03 01:12:08 +0300

    utils upd

--- advent_of_tasks/utils.go
+++ advent_of_tasks/utils.go
@@ -3,8 +3,21 @@ package adventoftasks
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func ScanIntoFIle(filepath string, day int) (*os.File, error) {
 	return os.Open(fmt.Sprintf("advent_of_tasks/src/%d/%s", day, filepath))
+}
+
+func stringsToInts(strings []string) ([]int, error) {
+	ints := make([]int, 0, len(strings))
+	for _, str := range strings {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			return nil, fmt.Errorf("ошибка преобразования строки '%s': %v", str, err)
+		}
+		ints = append(ints, num)
+	}
+	return ints, nil
 }
